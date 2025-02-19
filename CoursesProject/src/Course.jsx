da9e62ebfc1d1aa14111c6310e3261f153be5bd0
Ebru Bircan <ebrubircan2001@icloud.com>
2025-02-19 05:40:52 +0300

    first commit

---
+++ CoursesProject/src/Course.jsx
@@ -0,0 +1,10 @@
+import Angular from './assets/angular.png';
+import Bootstrap from './assets/bootstrap.png';
+import Ccsharp from './assets/ccsharp.png';
+import KompleWeb from './assets/kompleweb.png';
+
+function Course() {
+    return ( <div></div> );
+}
+
+export default Course;
