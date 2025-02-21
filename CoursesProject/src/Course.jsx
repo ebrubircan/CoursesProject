b936829606a3bc65389cd07005f93dcbafae2b2b
Ebru Bircan <ebrubircan2001@icloud.com>
2025-02-21 16:01:16 +0300

    Rastgele gelen kurslar için resimler eklendi

--- CoursesProject/src/Course.jsx
+++ CoursesProject/src/Course.jsx
@@ -3,9 +3,23 @@ import Bootstrap from './assets/bootstrap5.png';
 import Ccsharp from './assets/ccsharp.png';
 import KompleWeb from './assets/kompleweb.jpg';
 
-function Course( courseName) {
-    console.log(courseName);
-    return ( <div></div> );
+const courseMap = {
+    Angular,
+    Bootstrap,
+    Ccsharp,
+    KompleWeb
+    // Angular: Angular,
+    // Bootstrap: Bootstrap,
+    // Ccsharp: Ccsharp,
+    // KompleWeb: KompleWeb şeklinde de yazılabilirdi fakat key value değerleri aynı olduğu için tek taraflı yazıldı.
+    // const courseArray = [ 'Angular', 'Bootstrap', 'Ccsharp', 'KompleWeb' ]; buradaki key değerleri yukarıdakiler value değerleri
+}
+
+function Course( {courseName} ) {
+    console.log(courseMap[courseName]);
+    return ( <div>
+        <img src={courseMap[courseName]} alt="course" />
+    </div> );
 }
 
 export default Course;
