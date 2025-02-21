bedd93dbf3e5288eb59f14955a9da691d8f29863
Ebru Bircan <ebrubircan2001@icloud.com>
2025-02-21 15:43:56 +0300

    Rastgele Kurs Üretme

--- CoursesProject/src/App.jsx
+++ CoursesProject/src/App.jsx
@@ -1,13 +1,29 @@
 import { useState } from 'react'
+import Course from './Course'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-<>
-</>
-  )
+function getRandomCourse() {
+  const courseArray = [ 'Angular', 'Bootstrap', 'Ccsharp', 'KompleWeb' ];
+  return courseArray[Math.floor(Math.random() * courseArray.length)];
 }
 
+function App() {
+
+  const [courses, setCourses] = useState([])
+
+  const handleClick = () => {
+    setCourses([...courses, getRandomCourse()])
+  }
+
+  const courseList = courses.map((course, index) => (
+    <Course key={index} courseName={course} />
+  ))
+
+  return (
+    <div className="App">
+      <button onClick={handleClick}>Kurs Ekle</button>
+      {courseList}
+    </div>
+  );
+}
 export default App
--- CoursesProject/src/Course.jsx
+++ CoursesProject/src/Course.jsx
@@ -1,9 +1,10 @@
-import Angular from './assets/angular.png';
-import Bootstrap from './assets/bootstrap.png';
+import Angular from './assets/angular.jpg';
+import Bootstrap from './assets/bootstrap5.png';
 import Ccsharp from './assets/ccsharp.png';
-import KompleWeb from './assets/kompleweb.png';
+import KompleWeb from './assets/kompleweb.jpg';
 
-function Course() {
+function Course( courseName) {
+    console.log(courseName);
     return ( <div></div> );
 }
 
